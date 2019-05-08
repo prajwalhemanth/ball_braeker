c035f1a845c9f5b497fbb08617ed1385e2645d40
prajwalhemanth <45170874+prajwalhemanth@users.noreply.github.com>
2019-05-08 15:15:27 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+/*
+ * This is main class and it creates an object of BallBreaker Class
+ * 
+ */
+
+package ballbreaker;
+
+/**
+ *
+ * @author dinesh salve
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new BallBreaker();
+    }
+
+}
